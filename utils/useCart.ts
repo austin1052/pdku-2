26f4fb68622f4d0e1dfbc3c987db3ccec6af0375
austin1052 <austin1052@gmail.com>
2023-05-03 20:09:52 -0400

    remove useCart

--- utils/useCart.ts
+++
@@ -1,9 +0,0 @@
-function useCart(token: string) {
-  const { data, error, isLoading } = useSWR(`/api/user/${id}`, fetcher)
- 
-  return {
-    user: data,
-    isLoading,
-    isError: error
-  }
-}
